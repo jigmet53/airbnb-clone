6e7d9d7c1e762a394a346e771b3294a630c1fd12
KUNZES ANGMO <jigmetrinchen276@gmail.com>
2025-10-22 22:22:54 +0530

    mongoose implemented

---
+++ chapter17IntroToMongoose/controllers/store.js
@@ -0,0 +1,122 @@
+const Favourite = require("../modles/favourite");
+const Home = require("../modles/home");
+
+exports.getBooking= (req,res,next)=>{
+  res.render( 'store/booking',{pageTitle: 'Add Home to airbnb', currentPage: 'booking'})
+
+
+}
+exports.getReserve= (req,res,next)=>{
+  res.render( 'store/reserve',{pageTitle: 'Reserve page', currentPage: 'reserve'})
+
+
+}
+
+exports.getHomeDetails= (req,res,next)=>{
+  const homeId= req.params.homeId;
+  console.log("id", homeId);
+  Home.findById(homeId).then(home=>{
+
+   
+    if(!home)
+    {
+      res.redirect("/home");
+      console.log("error");
+    }
+    else{
+  res.render( 'store/home-details',{home:home,pageTitle: 'Home details', currentPage: 'home-details'})
+
+
+    }
+  })
+
+
+}
+
+
+exports.getFavtouteList = (req,res,next)=>{
+  Favourite.find().populate('houseId').then(favourite=>{
+    favouriteHome= favourite.map(fav=>fav.houseId);
+
+res.render('store/favourite-list',{favouriteHome:favouriteHome,  pageTitle: 'Favourite Home', currentPage: 'favourite-list'});
+
+ 
+
+
+  });
+}
+
+
+
+
+
+exports.postAddToFavourite = (req, res, next) => {
+  const homeId= req.body.id;
+Favourite.findOne({houseId: homeId}).then((fav)=>{
+  if(fav)
+  {
+  console.log("Already added ");
+    res.redirect("/favourite-list");
+  }
+  else{
+    fav = new Favourite({houseId: homeId});
+    fav.save().then((result)=>{
+console.log("Favourite added", result)
+    })
+
+  }
+    res.redirect("/favourite-list");
+
+
+
+}).catch(error=>{
+  console.log("Error while marking fav", error)
+})
+
+  
+
+
+}
+
+
+exports.postRemoveFromFavourite = (req, res, next) => {
+  const homeId = req.params.homeId;
+  Favourite.findOneAndDelete({houseId:homeId}).then(result=>{
+    console.log("Favourite removed", result);
+  }).catch(error=>{
+    console.log("Error fav", error);
+  
+  }).finally(()=>{
+    res.redirect("/favourite-list");
+
+  })
+   
+ 
+
+ 
+
+}
+
+exports.getHomes = (req,res,next)=>{
+Home.find().then (registeredHomes=>{
+  console.log("db data", registeredHomes)
+res.render('store/home-list',{registeredHomes:registeredHomes,  pageTitle: 'airbnb Home', currentPage: 'Home'});
+
+  });
+
+
+
+}
+
+exports.getIndex = (req,res,next)=>{
+Home.find().then (registeredHomes=>{
+
+
+res.render('store/index',{registeredHomes:registeredHomes,  pageTitle: 'airbnb index', currentPage: 'index'});
+
+  });
+
+
+
+}
+
---
+++ chapter17IntroToMongoose/modles/home.js
@@ -0,0 +1,48 @@
+const {ObjectId} = require('mongodb');
+const mongoose = require('mongoose');
+const Favourite= require('./favourite');
+
+/**
+     this.houseName= houseName;
+    this.price= price;
+    this.location=location;
+    this.rating= rating;
+    this.photoUrl= photoUrl;
+    this.description=description;
+    this._id=_id;
+  save (){
+
+  static find() {
+
+static findById(homeId){
+
+static deleteById(homeId, callback){
+
+
+
+
+
+ */
+
+
+const homeSchema=mongoose.Schema({
+  houseName:{type: String,required: true},
+  price:{type: Number,required: true},
+
+  location:{type: String,required: true},
+
+  rating:{type: Number,required: true},
+
+  photoUrl:String,
+
+  description:String,
+
+
+});
+homeSchema.pre('findOneAndDelete', async function(next){
+  const homeId = this.getQuery()["_id"];
+  await Favourite.deleteMany({houseId:homeId});
+  next();
+})
+
+module.exports =mongoose.model('Home',homeSchema);
